d3c5540f4f8a1f0778ef2b441d6f38d7eadfa543
Victorkib <victor3720kibiwott@gmail.com>
2023-11-08 08:46:41 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return <div className="app">App</div>;
+};
+
+export default App;
+//git config --global user.name "Victorkib"
+// git config --global user.email victor3720kibiwott@gmail.com
